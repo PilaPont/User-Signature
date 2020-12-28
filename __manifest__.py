c7d421f57c322ad6d656b629baf5b2f27ff1b1c8
Maryam Kia <maryam.kia1984@mail.com>
2020-12-28 14:36:52 +0330

    [MIG] letter_hr/: Migration to 14.0

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'User Signature',
+    'version': "14.0.1.0.0",
+    'category': '',
+    'description': """
+    User Signature
+    """,
+    'author': "Maryam Kia",
+    'website': "",
+    'depends': [],
+    'data': [
+        'views/res_users.xml',
+             ],
+    'demo': [],
+}
