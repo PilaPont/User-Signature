7fb287a7cc3c145297ed0c4659bb1f4a312eca39
Maryam Kia <maryam.kia1984@mail.com>
2021-08-27 17:19:44 +0000

    [Fix] letter, letter id doesn't show in assign wizard, set default for letter_id and edit do_save func.

--- models/res_users.py
+++ models/res_users.py
@@ -1,10 +1,41 @@
-__author__ = 'mkz'
+import io
+import base64
+from PIL import Image
+from math import isclose
 
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
 
 
 class ResUsers(models.Model):
     _inherit = "res.users"
     _description = "User Signature"
 
-    signature_image = fields.Binary(string="Signature Image")
+    signature_image = fields.Binary(string="Signature Image",
+                                    help="Upload user signature image. Please pay attention, the allowable aspect ratio"
+                                         " of signature image is 'one', with a maximum tolerance of 0.05!")
+
+    @api.model
+    def create(self, values):
+        signature_image = values.get('signature_image')
+        if signature_image:
+            self._check_signature_dimensions(signature_image)
+        result = super(ResUsers, self).create(values)
+        return result
+
+    def write(self, values):
+        for user in self:
+            signature_image = values.get('signature_image')
+            if signature_image:
+                user._check_signature_dimensions(signature_image)
+        result = super(ResUsers, self).write(values)
+        return result
+
+    @staticmethod
+    def _check_signature_dimensions(signature_image):
+        image_decoded = base64.decodebytes(signature_image.encode('utf-8'))
+        signature_image_string = io.BytesIO(image_decoded)
+        width, height = Image.open(signature_image_string).size
+        if not isclose(width, height, rel_tol=0.05):
+            raise UserError(
+                _("The allowable aspect ratio of signature image is 'one' with a maximum tolerance of 0.05!"))
